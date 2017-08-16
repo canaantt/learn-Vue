f04786e341f7fa77a48542cdf29b38ca7ad28a93
canaantt <canaantt@gmail.com>
2017-08-16 07:44:29 -0700

    login/out implemented

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -10,7 +10,10 @@
                     <router-link to="/FeedbackComponent" activeClass="active" tag="li"><a >Feedback</a></router-link>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                    <li v-if="user">{{ user.name }}</li>
+                    <li v-if="user">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img class="navbar-pic" :src="user.thumbnail" alt="Smiley face">
+                        </a>
                     <li>
                         <button v-if="isLoggedIn" class="btn btn-danger log" @click="googleLogout()">Log out </button>
                         <button v-if="!isLoggedIn" class="btn btn-info log" @click="googleLogin()">Log In</button>
@@ -80,7 +83,10 @@
                            self.$store.commit('getUser', data)
                          })
         })
-        hello.on('auth.logout', function () { alert('auth.logout!') })
+        hello.on('auth.logout', function () {
+          alert('auth.logout!')
+          self.$store.commit('getUser', null)
+        })
         hello.on('auth.change', function () { alert('state changed!') })
       }
     }
@@ -89,6 +95,84 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass">
   @import '../index.scss' 
-  .navbar-right { margin-right: 0px !important}
-  .log {margin: 5px 10px 0 0}
+</style>
+<style lang='css'>
+navbar {
+    border: none;
+    box-shadow: 0 2px 3px $color-gray;
+    font-family: $font-family;
+    font-weight: $font-weight;
+}
+
+.navbar-logo-img {
+    max-height: 40px;
+    image-rendering: crisp-edges;
+    margin-right: 10px;
+    margin-left: 60px;
+}
+
+.navbar-logo-text {
+    font-size: 18px;
+    line-height: 40px;
+    vertical-align: middle;
+    color: $color-white;
+    letter-spacing: 6px;
+    text-transform: uppercase;
+}
+
+.navbar-inverse {
+    border: none;
+    background-color: $color-onco-blue;
+}
+
+.navbar-inverse .navbar-toggle {
+    border: none
+}
+
+.navbar-items {
+    padding-right: 5px;
+    color: #fff;
+    margin-top: 12px;
+    font-size: 14px;
+    cursor: pointer;
+    span {
+        margin-right: 5px;
+        color: #fff;
+    }
+}
+
+// removing bootstrap carat over photo
+.dropdown-toggle::after {
+    display: none
+}
+
+.navbar-inverse .navbar-collapse,
+.navbar-inverse .navbar-form {
+    border-color: #0383c2;
+}
+
+.navbar-inverse .navbar-toggle {
+    border-color: #fff;
+}
+
+.navbar-inverse .navbar-nav>li>a {
+    color: $color-white;
+}
+
+.navbar-pic {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 10px;
+    margin-right: 60px;
+    cursor: pointer;
+}
+
+.dropdown-item {
+    font-size: 12px;
+}
+
+.dropdown-menu {
+    min-width: 8rem;
+}
 </style>
